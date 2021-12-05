3a0e2258910069c53cd778b64120536948fcb19f
bpkaman <bkaman293+github@gmail.com>
2021-12-05 13:49:33 -0600

    added ray class and readme

--- src/include/color.h
+++ src/include/color.h
@@ -11,6 +11,5 @@ void write_color(std::ostream &out, color pixel_color)
         << static_cast<int>(255.999 * pixel_color.y()) << ' '
         << static_cast<int>(255.999 * pixel_color.z()) << std::endl;
 }
-}
 
 #endif
