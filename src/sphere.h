c72d79ae350cc4897626c6d4296a645b47dbbb8e
bpkaman <bkaman293+github@gmail.com>
2021-12-07 20:52:55 -0600

    determine if rays are oriented with the surface normals

---
+++ src/sphere.h
@@ -0,0 +1,47 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "shape.h"
+#include "vec3.h"
+
+class Sphere : public Shape
+{
+    public:
+        Sphere() {}
+        Sphere(point3 cen, double r) : center(cen), radius(r) {};
+
+        virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+        public:
+            point3 center;
+            double radius;
+};
+
+bool Sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    vec3 oc = r.origin() - center;
+    double a = r.direction().length_squared();
+    double half_b = dot(oc, r.direction());
+    double c = oc.length_squared() - radius * radius;
+
+    double discriminant = half_b * half_b - a * c;
+    if (discriminant < 1.0) { return false; }
+    double sqrtd = sqrt(discriminant);
+
+    // Find the nearest root in the acceptable range
+    double root = (-half_b - sqrtd) / a;
+    if (root < t_min || t_max < root)
+    {
+        root = (-half_b + sqrtd) / a;
+        if (root < t_min || t_max < root) { return false; }
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
+    rec.normal = (rec.p - center) / radius;
+
+    return true;
+}
+
+#endif
